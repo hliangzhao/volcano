d07d723b575d9d3759669f63721f1c89e4b7d435
hliangzhao <hliangzhao@zju.edu.cn>
2022-01-17 21:17:27 +0800

    Add helpers

---
+++ pkg/scheduler/apis/helpers/helpers.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2021 hliangzhao.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helpers
+
+import (
+	"github.com/hliangzhao/volcano/pkg/scheduler/apis"
+	corev1 "k8s.io/api/core/v1"
+	"math"
+)
+
+func Min(l, r *apis.Resource) *apis.Resource {
+	res := &apis.Resource{}
+	res.MilliCPU = math.Min(l.MilliCPU, r.MilliCPU)
+	res.Memory = math.Min(l.Memory, r.Memory)
+
+	if l.ScalarResources == nil || r.ScalarResources == nil {
+		return res
+	}
+
+	res.ScalarResources = map[corev1.ResourceName]float64{}
+	for lName, lQuantity := range l.ScalarResources {
+		res.ScalarResources[lName] = math.Min(lQuantity, r.ScalarResources[lName])
+	}
+	return res
+}
+
+func Max(l, r *apis.Resource) *apis.Resource {
+	res := &apis.Resource{}
+	res.MilliCPU = math.Max(l.MilliCPU, r.MilliCPU)
+	res.Memory = math.Max(l.Memory, r.Memory)
+
+	if l.ScalarResources == nil || r.ScalarResources == nil {
+		return res
+	}
+
+	res.ScalarResources = map[corev1.ResourceName]float64{}
+	if l.ScalarResources != nil {
+		for lName, lQuantity := range l.ScalarResources {
+			if lQuantity > 0 {
+				res.ScalarResources[lName] = math.Max(lQuantity, r.ScalarResources[lName])
+			}
+		}
+	}
+	if r.ScalarResources != nil {
+		for rName, rQuantity := range r.ScalarResources {
+			if rQuantity > 0 {
+				res.ScalarResources[rName] = math.Max(rQuantity, res.ScalarResources[rName])
+			}
+		}
+	}
+	return res
+}
+
+func Share(l, r float64) float64 {
+	var share float64
+	if r == 0 {
+		if l == 0 {
+			share = 0
+		} else {
+			share = 1
+		}
+	} else {
+		share = l / r
+	}
+	return share
+}
---
+++ pkg/scheduler/apis/helpers/helpers_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2021 hliangzhao.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helpers
+
+import (
+	"github.com/hliangzhao/volcano/pkg/scheduler/apis"
+	corev1 "k8s.io/api/core/v1"
+	"reflect"
+	"testing"
+)
+
+func TestMax(t *testing.T) {
+	l := &apis.Resource{
+		MilliCPU: 1,
+		Memory:   1024,
+		ScalarResources: map[corev1.ResourceName]float64{
+			"gpu":    1,
+			"common": 4,
+		},
+	}
+	r := &apis.Resource{
+		MilliCPU: 2,
+		Memory:   1024,
+		ScalarResources: map[corev1.ResourceName]float64{
+			"npu":    2,
+			"common": 5,
+		},
+	}
+	expected := &apis.Resource{
+		MilliCPU: 2,
+		Memory:   1024,
+		ScalarResources: map[corev1.ResourceName]float64{
+			"gpu":    1,
+			"npu":    2,
+			"common": 5,
+		},
+	}
+	re := Max(l, r)
+	if !reflect.DeepEqual(expected, re) {
+		t.Errorf("expected: %#v, got: %#v", expected, re)
+	}
+}
